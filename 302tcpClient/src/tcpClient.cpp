c11d9c51bb43d7a8a1301c6f4cd728ba6dc0aef2
Oğuz Ceylan <postaoguz@gmail.com>
2025-07-15 18:06:46 +0300

    Tweaked 302tcpClient/src/tcpClient.cpp

--- 302tcpClient/src/tcpClient.cpp
+++ 302tcpClient/src/tcpClient.cpp
@@ -7,8 +7,6 @@
 #include <string.h>
 #include <string>
 
-using namespace std;
-
 int main()
 {
     //	Create a socket
@@ -20,7 +18,7 @@ int main()
 
     //	Create a hint structure for the server we're connecting with
     int port = 54000;
-    string ipAddress = "127.0.0.1";
+    std::string ipAddress = "127.0.0.1";
 
     sockaddr_in hint;
     hint.sin_family = AF_INET;
@@ -36,19 +34,18 @@ int main()
 
     //	While loop:
     char buf[4096];
-    string userInput;
-
+    std::string userInput;
 
     do {
         //		Enter lines of text
-        cout << "> ";
-        getline(cin, userInput);
+        std::cout << "> ";
+        std::getline(std::cin, userInput);
 
         //		Send to server
         int sendRes = send(sock, userInput.c_str(), userInput.size() + 1, 0);
         if (sendRes == -1)
         {
-            cout << "Could not send to server! Whoops!\r\n";
+            std::cout << "Could not send to server! Whoops!\r\n";
             continue;
         }
 
@@ -57,12 +54,12 @@ int main()
         int bytesReceived = recv(sock, buf, 4096, 0);
         if (bytesReceived == -1)
         {
-            cout << "There was an error getting response from server\r\n";
+            std::cout << "There was an error getting response from server\r\n";
         }
         else
         {
             //		Display response
-            cout << "SERVER> " << string(buf, bytesReceived) << "\r\n";
+            std::cout << "SERVER> " << std::string(buf, bytesReceived) << "\r\n";
         }
     } while(true);
 
